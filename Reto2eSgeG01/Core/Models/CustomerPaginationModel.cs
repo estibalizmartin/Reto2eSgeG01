ba63674d9c2f5b220482366653bf40d0dbece7fc
RodyArmoa <59371352+RodyArmoa@users.noreply.github.com>
2023-02-02 18:03:23 +0100

    Se realiza paginacion casi completa.

---
+++ Reto2eSgeG01/Core/Models/CustomerPaginationModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Reto2eSgeG01.Core.Models
+{
+    public class CustomerPaginationModel
+    {
+        public string CustomerId { get; set; } = null!;
+        public string CompanyName { get; set; } = null!;
+        public string? ContactName { get; set; }
+    }
+}
---
+++ Reto2eSgeG01/Core/Models/Paginacion.cs
@@ -0,0 +1,7 @@
+﻿namespace Reto2eSgeG01.Core.Models
+{
+    public enum Paginacion
+    {
+        a=5,b=10,c=25
+    }
+}
